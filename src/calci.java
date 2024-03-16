d9d7b62f2a5b3e152b93e9748076b849c13445e3
root <root@ip-172-31-20-132.ec2.internal>
2024-03-16 12:35:26 +0000

    This is First Commit

---
+++ src/calci.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+package defaul
+class calci {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
